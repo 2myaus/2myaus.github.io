d25ddadd5d9baf7bc24c707c30c38231ac5cb6f2
Daniil Petrov <waddlespinespig@gmail.com>
2024-05-16 18:30:10 -0400

    opened pandora's box

--- windows.js
+++ windows.js
@@ -3,21 +3,45 @@
 	windows.forEach((window_n) => {
 		const closebutton = window_n.querySelectorAll(".windowhead .button.close")[0];
 		closebutton.onclick = () => {
-			const sadface = document.createElement("div");
-			sadface.innerText = ":(";
-			sadface.style = `
-				font-family:Kitty-M;
-				font-size: 10vw;
-				transform-origin:center center;
-				position:absolute;
-				left:50%;
-				top:50%;
-				transform:translate(-50%, -50%);
-			`;
+			window_n.classList.add("closed");
+
 			setTimeout(() => {
-				sadface.remove();
-			}, 1500);
-			window_n.appendChild(sadface);
+				window_n.classList.remove("closed");
+
+				const darkoverlay = document.createElement("div");
+				darkoverlay.style = `
+					z-index:9;
+					position:absolute;
+					top:0;
+					left:0;
+					width:100%;
+					height:100%;
+					background-color:#0008;
+					transition: opacity 0.4s linear;
+					opacity:1;
+				`;
+				window_n.appendChild(darkoverlay);
+
+				const sadface = document.createElement("img");
+				sadface.style = `
+					transform-origin:center center;
+					position:absolute;
+					left:50%;
+					top:50%;
+					transform:translate(-50%, -50%);
+					z-index:10;
+				`;
+				sadface.src = "nso_sad.webp";
+
+				darkoverlay.appendChild(sadface);
+				setTimeout(() => {
+					darkoverlay.style.opacity = 0;
+				}, 1100);
+
+				setTimeout(() => {
+					darkoverlay.remove();
+				}, 1500);
+			}, Math.random() * 800 + 400);
 		}
 	});
 })();
